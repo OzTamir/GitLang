8adc32a3456d6c115067e4f359d322f80c290c71
Oz Tamir <OzTamir@server.oztamir.com>
2014-04-07 21:40:38 +0300

    Added files

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 GitLang
-=======
+=========
 
-Visualize the languges used in a certin GitHub Repository
+Just a little tool I wrote in Javascript to display a bar chart of the languges in a certin GitHub Repository.
+
+Tools in use:
+---
+ - [ZURB's Foundation] (https://github.com/zurb/foundation) For HTML and responsiveness across devices.
+ - [move.js] (https://github.com/visionmedia/move.js) For the transitions.
+ - [animate.css] (https://github.com/daneden/animate.css) For some more transitions.
+ - [d3] (https://github.com/mbostock/d3) For the bar charts...
+ - [GitHub API] (https://developer.github.com/v3/) For the data
+Copyright 2014 Oz Tamir
---
+++ main.js
@@ -0,0 +1,111 @@
+var repoChart = function repoChart(user, repo){
+
+var url = "https://api.github.com/repos/"
+url += user + '/' + repo + '/languages';
+
+//"danasilver/danasilver.org/languages";
+d3.json(url, makeChart);
+
+function makeChart(languageJSON) {
+
+  // Format the data to work with D3
+  var dataArray = [],
+      dataKeyVal = [];
+
+  for (lang in languageJSON) {
+    dataArray.push(languageJSON[lang]);
+  }
+
+  for (lang in languageJSON) {
+    dataKeyVal.push({"lang": lang, "bytes": languageJSON[lang]});
+  }
+
+	 var margin = {top: 10, right: 20, bottom: 20, left: 60},
+	    width = 500 - margin.left - margin.right,
+	    height = 400 - margin.top - margin.bottom;
+
+	var x = d3.scale.ordinal()
+	    .domain(dataKeyVal.map(function(d) { return d.lang; } ))
+	    .rangeRoundBands([0, width], .1);
+
+	var y = d3.scale.linear()
+	    .domain([0, d3.max(dataKeyVal, function(d) { return d.bytes })])
+	    .range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
+
+	var chart = d3.select(".vertical-bar-chart")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	chart.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis);
+
+	chart.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+
+	d3.select(".vertical-bar-chart").append("text")
+	    .style("text-anchor", "end")
+	    .attr("transform", "translate(15," + height / 2 + "), rotate(-90)")
+	    .text("Bytes");
+
+	var bar = chart.selectAll(".bar")
+	    .data(dataKeyVal)
+	  .enter().append("g")
+	    .attr("class", "bar")
+	    .attr("transform", function(d) { return "translate(" + x(d.lang) + ",0)"; })
+
+	bar.append("rect")
+	    .attr("class", "bar")
+	    .attr("y", function(d) { return y(d.bytes); })
+	    .attr("height", function(d) { return height - y(d.bytes); })
+	    .attr("width", x.rangeBand())
+
+	bar.append("text")
+	    .attr("x", x.rangeBand() / 2)
+	    .attr("y", function(d) { return y(d.bytes) + 3; })
+	    .attr("dy", ".75em")
+	    .text(function(d) { return d.bytes; });
+
+}
+};
+
+var createList = function(data){
+	var list = '';
+	for(var i = 0; i < data.length; i++) {
+		var href = "<a href='#" + data[i] + "' id='repoNum" + i + "'>" + data[i] + "</a>";
+		console.log(href);
+		if(i == data.length - 1){
+			href += '|'
+		};
+		list += href
+	}
+	$('#menu').html(repoList);
+	repoChart(user, repos[0]);
+	console.log(list);
+	return list;
+};
+
+var getRepos = function(username){
+	var repos = new Array();
+	$.getJSON('https://api.github.com/users/' + username + '/repos', function(data) {
+	    for (var i = 0; i < data.length; i++) { 
+		    repos.push(data[i].name);
+		    console.log(data[i].name);
+		};
+	});
+	var repoList = createList(repos);
+	return repos;
+};
+
